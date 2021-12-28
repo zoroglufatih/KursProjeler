ec8314a7f765548799754ac4a2429c9c4a46a776
fatihzoroglu <fatihzorogluu@gmail.com>
2021-12-28 15:51:23 +0300

    profil güncelle şifre güncelle eklendi

--- 10-MVC/WM1_WebApp/ItServiceApp/Controllers/AccountController.cs
+++ 10-MVC/WM1_WebApp/ItServiceApp/Controllers/AccountController.cs
@@ -1,12 +1,16 @@
-﻿using ItServiceApp.Models;
+﻿using ItServiceApp.Extensions;
+using ItServiceApp.Models;
 using ItServiceApp.Models.Identity;
 using ItServiceApp.Services;
 using ItServiceApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using System;
 using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace ItServiceApp.Controllers
@@ -80,16 +84,23 @@ namespace ItServiceApp.Controllers
             if (result.Succeeded)
             {
                 var count = _userManager.Users.Count();
-                result = await _userManager.AddToRoleAsync(user, count == 1 ? RoleModels.Admin : RoleModels.User);
-
-                await _emailSender.SendAsync(new EmailMessage()
-                {
-                    Contacts = new string[] { model.Email },
-                    Subject = $"{model.UserName} - Kullanıcı Kayıt Yaptı",
-                    Body = $"Sn. {model.Name} {model.Surname} sitemize {DateTime.Now:g} itibari ile sitemize kayıt yaptınız. Kayıt yaptığınız için teşekkür ederiz."
-                });
                 //kullanıcıya rol atama
+                result = await _userManager.AddToRoleAsync(user, count == 1 ? RoleModels.Admin : RoleModels.Passive);
+
                 //email onay maili
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol:Request.Scheme);
+
+                var emailMessage = new EmailMessage()
+                {
+                    Contacts = new string[] { user.Email },
+                    Body = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Clicking here</a>.",
+                    Subject = "Confirm your email"
+                };
+
+                await _emailSender.SendAsync(emailMessage);
+
                 //login sayfasına yönlendirme
                 return RedirectToAction("Login", "Account");
             }
@@ -99,6 +110,35 @@ namespace ItServiceApp.Controllers
                 return View(model);
             }
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if(userId == null || code == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if(user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userId}'.");
+            }
+
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            ViewBag.StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+
+            if(result.Succeeded && _userManager.IsInRoleAsync(user, RoleModels.Passive).Result)
+            {
+                await _userManager.RemoveFromRoleAsync(user, RoleModels.Passive);
+                await _userManager.AddToRoleAsync(user, RoleModels.User);
+            }
+
+            return View();
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
@@ -118,12 +158,12 @@ namespace ItServiceApp.Controllers
             {
                 var user = await _userManager.FindByNameAsync(model.UserName);
 
-                await _emailSender.SendAsync(new EmailMessage()
-                {
-                    Contacts = new string[] { "fatih.zorogluu@gmail.com" },
-                    Subject = $"{user.UserName} - Kullanıcı Giriş Yaptı",
-                    Body = $"{user.Name} {user.Surname} isimli kullanıcı {DateTime.Now:g} itibari ile siteye giriş yapmıştr."
-                });
+                //await _emailSender.SendAsync(new EmailMessage()
+                //{
+                //    Contacts = new string[] { "fatih.zorogluu@gmail.com" },
+                //    Subject = $"{user.UserName} - Kullanıcı Giriş Yaptı",
+                //    Body = $"{user.Name} {user.Surname} isimli kullanıcı {DateTime.Now:g} itibari ile siteye giriş yapmıştr."
+                //});
 
                 return RedirectToAction("Index", "Home");
             }
@@ -141,6 +181,89 @@ namespace ItServiceApp.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var user = await _userManager.FindByIdAsync(HttpContext.GetUserId());
+
+            var model = new UserProfileViewModel()
+            {
+                Email = user.Email,
+                Name = user.Name,
+                Surname = user.Surname
+            };
+
+            return View(model);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Profile(UserProfileViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(HttpContext.GetUserId());
+
+            user.Name = model.Name;
+            user.Surname = model.Surname;
+            if(user.Email != model.Email)
+            {
+                await _userManager.RemoveFromRoleAsync(user, RoleModels.User);
+                await _userManager.RemoveFromRoleAsync(user, RoleModels.Passive);
+                user.Email = model.Email;
+
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Scheme);
+
+                var emailMessage = new EmailMessage()
+                {
+                    Contacts = new string[] { user.Email },
+                    Body = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Clicking here</a>.",
+                    Subject = "Confirm your email"
+                };
+
+                await _emailSender.SendAsync(emailMessage);
+            }
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, ModelState.ToFullErrorString());
+            }
+
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult UpdatePassword()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> UpdatePassword(PasswordChangeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByIdAsync(HttpContext.GetUserId());
+
+            var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+
+            if (result.Succeeded)
+            {
+                ViewBag.Message = "Parola güncelleme işlemi başarılı";
+            }
+            else
+            {
+                ViewBag.Message = $"Bir Hata oluştu {ModelState.ToFullErrorString()}";
+            }
+
+            return RedirectToAction("Profile");
         }
     }
 }
---
+++ 10-MVC/WM1_WebApp/ItServiceApp/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItServiceApp.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Required(ErrorMessage = "Eski Şifre alanı gereklidir.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karakterli olmalıdır!")]
+        [Display(Name = "Eski Şifre")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+        [Required(ErrorMessage = "Yeni Şifre alanı gereklidir.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karakterli olmalıdır!")]
+        [Display(Name = "Yeni Şifre")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Şifre tekrar alanı gereklidir.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Yeni Şifre tekrar")]
+        [Compare(nameof(NewPassword), ErrorMessage = "Şifreler uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
